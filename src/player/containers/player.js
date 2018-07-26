c7127fdb7faad593288f3f14a44b135a0ce70989
Leidy-Zulu <leidyzulu9@gmail.com>
2018-07-25 19:12:16 -0500

    Buffer media player

--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,23 +1,44 @@
 import React,  {Component  } from "react";
 import {
-    StyleSheet
+    StyleSheet,
+    ActivityIndicator
 } from 'react-native'
 import Video from 'react-native-video';
 import Layout from '../../player/components/layout';
 
 class Player extends Component{
+    state = {
+        loading: true,
+    }
+    onBuffer = ({isBuffering}) => {
+        this.setState({
+            loading: isBuffering
+        })
+    }
+    onLoad = () => {
+        this.setState({
+            loading: false
+        })
+    }
+
 render(){
     return(
     <Layout
+      loading={this.state.loading}
       video={
         <Video
         source={{uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4' }}
         style={styles.video}
         resizeMode="contain"
+        volume={0}
+        onBuffer={this.onBuffer}
+        onLoad={this.onLoad}
         />
       }
-    >
-    </Layout>
+      loader={
+          <ActivityIndicator color="red"/>
+      }
+    />
     )
 }
 }
@@ -29,7 +50,6 @@ const styles = StyleSheet.create({
         right: 0,
         bottom: 0,
         top: 0,
-        backgroundColor: 'black'
     }
 })
 
